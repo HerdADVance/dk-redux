9ee5bf966e3176ee166e6b164f69303610703fc5
Alex <avance@bulldogcreative.com>
2020-01-22 16:54:58 -0500

    Slot display

--- src/components/Slot.js
+++ src/components/Slot.js
@@ -4,8 +4,8 @@ import React from 'react';
 const Slot = ({ slot = {}, lineupId }) => { // pass removeSlot here later
   return (
     <tr data-pos={slot.position}>
+      <td>{slot.position}</td>
       <td>{slot.player}</td>
-      <td></td>
       <td></td>
       <td></td>
       {/*MoveSlotToListContainer slotId={slot.id} listId={listId} />*/}
